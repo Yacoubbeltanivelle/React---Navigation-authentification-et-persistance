984abd9a12ac29430bb5f8d33bed195f2714f8cf
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-05-12 16:40:57 +0200

    container-Marges

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,23 +5,25 @@ import { Link } from "react-router";
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">
-              Accueil
-            </Nav.Link>
-            <Nav.Link as={Link} to="/category/vetements">
-              Vêtements
-            </Nav.Link>
-            <Nav.Link as={Link} to="/category/chaussures">
-              Chaussures
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">
+                Accueil
+              </Nav.Link>
+              <Nav.Link as={Link} to="/category/vetements">
+                Vêtements
+              </Nav.Link>
+              <Nav.Link as={Link} to="/category/chaussures">
+                Chaussures
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,17 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router";
+import Container from "react-bootstrap/Container";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <Container>
+        <main className="my-4">
+          <Outlet />
+        </main>
+      </Container>
       <Footer />
     </>
   );
