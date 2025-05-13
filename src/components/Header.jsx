d3352eaaf0bf6e3be66a78fc3ffb3989452ad1da
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-05-13 13:32:07 +0200

    ACTIVITE 7 : Utiliser le hook useMatch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,21 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router";
 import "../assets/Header.css";
+import { useMatch } from "react-router";
 
 function Header() {
+  const match = useMatch("category/:categoryId");
+
   return (
     <header>
       <Navbar expand="lg lo" className="bg-body-tertiary">
         <Container>
+          {match ? (
+            <h2>Catégorie : {match.params.categoryId}</h2>
+          ) : (
+            <h2>Bienvenue sur le site</h2>
+          )}
+
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link as={NavLink} to="/">
