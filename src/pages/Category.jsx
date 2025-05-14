3fd40b7976c77f573f1eef9caebaa37b232cec72
Yacoubbeltanivelle <yacoubbeltanivelle@gmail.com>
2025-05-14 10:23:36 +0200

    ACTIVITE 8 : Retourner à la page d’accueil

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router";
 
 function Category() {
   const { categoryId } = useParams();
+  const navigate = useNavigate();
+
+  const returnHome = () => {
+    navigate("/");
+  };
   return (
     <div>
       <h1>Détails du blog avec l'ID : {categoryId}</h1>
+      <Button onClick={returnHome} variant="primary">
+        Retour à la page d'accueil
+      </Button>
     </div>
   );
 }
